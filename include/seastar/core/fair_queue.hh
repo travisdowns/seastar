e9b2b606d7fbce87ad4ed5bb6fac80ba4015e15d
John Spray <jcs@vectorized.io>
2021-08-31 14:00:57 +0100

    core: use 64 bit head+tail pointers in fair_queue
    When uint32_t offsets were used here, the frequent
    integer wrapping caused queues to get stuck.  The _pending
    tail values would compare as greater than the head values
    forever if there weren't always new requests coming through
    to wrap the head/tail pointers again.
    
    Signed-off-by: John Spray <jcs@vectorized.io>

--- include/seastar/core/fair_queue.hh
+++ include/seastar/core/fair_queue.hh
@@ -94,11 +94,11 @@ public:
 };
 
 class fair_group_rover {
-    uint32_t _weight = 0;
-    uint32_t _size = 0;
+    uint64_t _weight = 0;
+    uint64_t _size = 0;
 
 public:
-    fair_group_rover(uint32_t weight, uint32_t size) noexcept;
+    fair_group_rover(uint64_t weight, uint64_t size) noexcept;
 
     /*
      * For both dimentions checks if the current rover is ahead of the
@@ -143,7 +143,6 @@ public:
 /// the given time frame exceeds the disk throughput.
 class fair_group {
     using fair_group_atomic_rover = std::atomic<fair_group_rover>;
-    static_assert(fair_group_atomic_rover::is_always_lock_free);
 
     fair_group_atomic_rover _capacity_tail;
     fair_group_atomic_rover _capacity_head;
