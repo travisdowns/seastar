baaccf106330b6ce4fb50eba40e043985d9ab8d4
Travis Downs <travis.downs@gmail.com>
2022-04-09 12:41:22 -0700

    Fix escaping of regexes
    The existing regexes didn't escape backslash correctly:
    using say '\S' rather than '\\S' or r'\S'. This seems to mostly
    have been working by luck as if an escape character isn't
    recognized by python it will pass through the invalid
    escape backslash and all.

--- scripts/addr2line.py
+++ scripts/addr2line.py
@@ -92,15 +92,15 @@ class BacktraceResolver(object):
 
         def __init__(self):
             addr = "0x[0-9a-f]+"
-            path = "\S+"
-            token = f"(?:{path}\+)?{addr}"
-            full_addr_match = f"(?:(?P<path>{path})\+)?(?P<addr>{addr})"
-            self.oneline_re = re.compile(f"^((?:.*(?:(?:at|backtrace):?|:))?(?:\s+))?({token}(?:\s+{token})*)(?:\).*|\s*)$", flags=re.IGNORECASE)
+            path = r"\S+"
+            token = fr"(?:{path}\+)?{addr}"
+            full_addr_match = fr"(?:(?P<path>{path})\+)?(?P<addr>{addr})"
+            self.oneline_re = re.compile(fr"^((?:.*(?:(?:at|backtrace):?|:))?(?:\s+))?({token}(?:\s+{token})*)(?:\).*|\s*)$", flags=re.IGNORECASE)
             self.address_re = re.compile(full_addr_match, flags=re.IGNORECASE)
-            self.asan_re = re.compile(f"^(?:.*\s+)\({full_addr_match}\)\s*$", flags=re.IGNORECASE)
-            self.asan_ignore_re = re.compile(f"^=.*$", flags=re.IGNORECASE)
-            self.syslog_re = re.compile(f"^(?:.*\s+){full_addr_match}\s*$", flags=re.IGNORECASE)
-            self.separator_re = re.compile('^\W*-+\W*$')
+            self.asan_re = re.compile(fr"^(?:.*\s+)\({full_addr_match}\)\s*$", flags=re.IGNORECASE)
+            self.asan_ignore_re = re.compile(fr"^=.*$", flags=re.IGNORECASE)
+            self.syslog_re = re.compile(fr"^(?:.*\s+){full_addr_match}\s*$", flags=re.IGNORECASE)
+            self.separator_re = re.compile(r'^\W*-+\W*$')
 
         def __call__(self, line):
             def get_prefix(s):
@@ -267,4 +267,3 @@ class BacktraceResolver(object):
         else:
             print(f"Unknown '{line}': {res}")
             raise RuntimeError("Unknown result type {res}")
-
