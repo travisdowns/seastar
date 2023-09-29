4cd5f1173cd855bb82e02a28f3ac6e82d8304122
Michael Boquard <michael@redpanda.com>
2023-09-29 08:39:29 -0400

    httpd: Added server and client addresses to request structure
    Signed-off-by: Michael Boquard <michael@redpanda.com>
    (cherry picked from commit 7ee2796957d32536f410002c852a7ba4e906aafc)

--- include/seastar/http/httpd.hh
+++ include/seastar/http/httpd.hh
@@ -76,6 +76,8 @@ class connection : public boost::intrusive::list_base_hook<> {
     connected_socket _fd;
     input_stream<char> _read_buf;
     output_stream<char> _write_buf;
+    socket_address _client_addr;
+    socket_address _server_addr;
     static constexpr size_t limit = 4096;
     using tmp_buf = temporary_buffer<char>;
     http_request_parser _parser;
@@ -91,8 +93,26 @@ public:
     connection(http_server& server, connected_socket&& fd, socket_address, bool tls, int listener_idx)
             : connection(server, std::move(fd), tls, listener_idx) {}
     connection(http_server& server, connected_socket&& fd, bool tls, int listener_idx)
-            : _server(server), _fd(std::move(fd)), _read_buf(_fd.input()), _write_buf(
-                    _fd.output()), _tls(tls), _listener_idx(listener_idx) {
+            : _server(server)
+            , _fd(std::move(fd))
+            , _read_buf(_fd.input())
+            , _write_buf(_fd.output())
+            , _client_addr(_fd.remote_address())
+            , _server_addr(_fd.local_address())
+            , _tls(tls)
+            , _listener_idx(listener_idx) {
+        on_new_connection();
+    }
+    connection(http_server& server, connected_socket&& fd, bool tls, int listener_idx,
+            socket_address client_addr, socket_address server_addr)
+            : _server(server)
+            , _fd(std::move(fd))
+            , _read_buf(_fd.input())
+            , _write_buf(_fd.output())
+            , _client_addr(std::move(client_addr))
+            , _server_addr(std::move(server_addr))
+            , _tls(tls)
+            , _listener_idx(listener_idx) {
         on_new_connection();
     }
     ~connection();
