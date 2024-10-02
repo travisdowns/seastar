e86a47983422d99dfb665a13d1782d57116dc381
Brandon Allard <brandon@redpanda.com>
2024-10-01 22:47:07 -0400

    io_queue: add smp header to get full smp def

--- src/core/io_queue.cc
+++ src/core/io_queue.cc
@@ -37,6 +37,7 @@
 #include <seastar/core/io_queue.hh>
 #include <seastar/core/io_intent.hh>
 #include <seastar/core/reactor.hh>
+#include <seastar/core/smp.hh>
 #include <seastar/core/when_all.hh>
 #include <seastar/core/metrics.hh>
 #include <seastar/core/internal/io_desc.hh>
